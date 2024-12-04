7211d634e2c1727f243e5c9e87909ee451951f11
Connor Pecar <cpecar@ifarm2402.jlab.org>
2024-12-04 16:33:23 -0500

    Objective calculation script (later: use mchi2 from certain PMTs, etc.)

---
+++ Clas12RichUtils/runObjectiveCalc.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os, sys, subprocess, time
+import math
+import uncertainties
+import uproot
+
+def retrieveResults(job_id):
+    # when results finished, retrieve analysis script outputs
+    # and calculate objectives
+    rootfile = uproot.open(str(os.environ["AIDE_HOME"])+"/rich/log/root_files/output_{}.root".format(job_id))
+    tree = rootfile[rootfile.keys()[0]]
+    mchi2 = tree["mchi2"].array(library='np')    
+    return np.mean(mchi2)
+
+jobid = sys.argv[1]
+
+meanchi2 = np.array(retrieveResults(jobid))
+np.savetxt(os.environ["AIDE_HOME"]+"/log/results/" + "rich-align-mobo-out_{}.txt".format(jobid),[meanchi2])

22502db2ae8973b766a923ac3907232e59c76919
Connor Pecar <cpecar@ifarm2402.jlab.org>
2024-12-05 16:13:10 -0500

    Place all ccdb editing and reconstruction in one bash script

--- ProjectUtils/slurm_utilities.py
+++ ProjectUtils/slurm_utilities.py
@@ -30,19 +30,7 @@ class SlurmQueueClient:
             file.write("#SBATCH --time=1:00:00\n") #CHECK HOW LONG IS REALLY NEEDED
             file.write("#SBATCH --output="+str(os.environ["AIDE_HOME"])+"/log/job_output/drich-mobo_%j.out\n")
             file.write("#SBATCH --error="+str(os.environ["AIDE_HOME"])+"/log/job_output/drich-mobo_%j.err\n")
-            #file.write("module use /scigroup/cvmfs/hallb/clas12/sw/modulefiles\n")
-            #TODO: just put this all in a bash script...
-            #file.write("module load clas12 \n")
-            file.write("export CCDB_CONNECTION=sqlite:///"+str(os.environ["AIDE_HOME"])+"/ccdb_copy.sqlite\n")
-            file.write("echo $PYTHONPATH \n")
-            file.write('if [ -f "'+str(os.environ["AIDE_HOME"])+'/rich/log/hipo_files/output_{}.hipo" ]; then\n'.format(jobnum))
-            file.write('    rm "'+str(os.environ["AIDE_HOME"])+'/rich/log/hipo_files/output_{}.hipo"\n'.format(jobnum))
-            file.write('fi\n')
-            file.write("ccdb mkvar variation_{}\n".format(jobnum))
-            file.write("ccdb add /geometry/rich/module1/alignment -v variation_{} ".format(jobnum)+str(os.environ["AIDE_HOME"])+"/rich/tables/rich_m1_alignment_{}.dat\n".format(jobnum))
-            file.write("recon-util -y "+str(os.environ["AIDE_HOME"])+"/rich/yaml/rich_{}.yaml -i ".format(jobnum)+str(os.environ["AIDE_HOME"])+"/rich_skim.hipo -o "+str(os.environ["AIDE_HOME"])+"/rich/log/hipo_files/output_{}.hipo\n".format(jobnum))
-            file.write(str(os.environ["AIDE_HOME"])+"/Clas12RichUtils/RICH-track-matching-tree "+str(os.environ["AIDE_HOME"])+"/rich/log/root_files/output_{} ".format(jobnum)+str(os.environ["AIDE_HOME"])+"/rich/log/hipo_files/output_{}.hipo\n".format(jobnum))
-            file.write("python " + str(os.environ["AIDE_HOME"])+"/Clas12RichUtils/"+"runObjectiveCalc.py {} \n".format(jobnum))
+            file.write(str(os.environ["AIDE_HOME"])+'/Clas12RichUtils/runReconstruction.sh {}'.format(jobnum))
         print("starting slurm job ", jobnum)
         shellcommand = ["sbatch","--export=ALL","jobconfig_{}.slurm".format(jobnum)]        
         commandout = subprocess.run(shellcommand,stdout=subprocess.PIPE)

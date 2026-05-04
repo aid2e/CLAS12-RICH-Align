13118b622fbca4387081561e14d8ff1e46a0c13f
Connor Pecar <pecar.connor@gmail.com>
2026-05-04 15:27:10 -0400

    Place skimmed files in subdirectories for each alignment step

--- merge_datasets.sh
+++ merge_datasets.sh
@@ -7,7 +7,5 @@ fi
 indir="$1"
 
 # Produce two needed merged hipo files for global and Cherenkov angle alignment
-hipo-utils -merge -o "$indir/merged_dataset_globalalign.hipo" "$indir"/*clusters*.hipo
-hipo-utils -merge -o "$indir/merged_dataset_cherenkovalign.hipo" "$indir"/*minp*.hipo
-# using current naming conventions from configuration script,
-# would be more readable if just placed in different subdirectories
+hipo-utils -merge -o "$indir/merged_dataset_globalalign.hipo" "$indir"/skim_clusters/*
+hipo-utils -merge -o "$indir/merged_dataset_cherenkovalign.hipo" "$indir"/skim_cherenkov/*
